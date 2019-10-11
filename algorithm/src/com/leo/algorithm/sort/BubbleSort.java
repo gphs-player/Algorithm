083b5c7ddb68b15adb236f51827dd2c5311a3e6a
niubao <niubao@2dfire.com>
2019-10-11 11:28:10 +0800

    冒泡排序学习

---
+++ algorithm/src/com/leo/algorithm/sort/BubbleSort.java
@@ -0,0 +1,109 @@
+package com.leo.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 排序算法
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) throws InterruptedException {
+        int[] arr = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
+//        bubbleSort(arr);
+//        bubbleSort2(arr);
+        int[] arr3 = new int[]{3, 4, 2, 1, 5, 6, 7, 8};
+        bubbleSort3(arr3);
+//        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int arr[]) {
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                count++;
+                int temp;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+                /*
+                [5, 6, 3, 8, 2, 1, 7, 9]
+                [5, 3, 6, 2, 1, 7, 8, 9]
+                [3, 5, 2, 1, 6, 7, 8, 9]
+                [3, 2, 1, 5, 6, 7, 8, 9]
+                [2, 1, 3, 5, 6, 7, 8, 9]
+                [1, 2, 3, 5, 6, 7, 8, 9]
+                [1, 2, 3, 5, 6, 7, 8, 9]//最后一轮是不必要的，因为已经有序了
+                */
+        }
+        System.out.println(count);
+    }
+
+    /**
+     * 如果排序到最后若干轮，列表已经有序，如何优化？
+     */
+    private static void bubbleSort2(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean sorted = true;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                int temp;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    sorted = false;//如果本轮循环有元素交换，说明无序，否则就是有序了，跳出大循环
+                }
+            }
+            if (sorted) {//避免不必要排序
+                break;
+            }
+            System.out.println(Arrays.toString(arr));
+            /*
+            [5, 6, 3, 8, 2, 1, 7, 9]
+            [5, 3, 6, 2, 1, 7, 8, 9]
+            [3, 5, 2, 1, 6, 7, 8, 9]
+            [3, 2, 1, 5, 6, 7, 8, 9]
+            [2, 1, 3, 5, 6, 7, 8, 9]
+            [1, 2, 3, 5, 6, 7, 8, 9]//减少一轮循环
+            * */
+        }
+    }
+
+
+    /**
+     * 如果数组的后半段是有序的，怎么避免每次循环都进行无用的比较
+     */
+    private static void bubbleSort3(int arr[]) {
+        int count = 0;
+        int lastExchangeIndex = 0;
+        int sortBorder = arr.length - 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean sorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                count++;
+                int temp;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    sorted = false;//如果本轮循环有元素交换，说明无序，否则就是有序了，跳出大循环
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            if (sorted) {//避免不必要排序
+                break;
+            }
+            System.out.println(Arrays.toString(arr));
+            /* 仅比较10次就能得出结果，如果用前两次的方法，要比较28次
+            [3, 2, 1, 4, 5, 6, 7, 8]
+            [2, 1, 3, 4, 5, 6, 7, 8]
+            [1, 2, 3, 4, 5, 6, 7, 8]
+            * */
+        }
+        System.out.println(count);
+    }
+}
---
+++ algorithm/src/com/leo/algorithm/sort/CocktailSort.java
@@ -0,0 +1,23 @@
+package com.leo.algorithm.sort;
+
+/**
+ * 鸡尾酒排序
+ * 冒泡排序的优化
+ *
+ * 在数组序列大部分有序的情况下，冒泡排序似乎有点憋屈。
+ * 比如【2，3，4，5，6，7，8，1】
+ * 应用场景也是比较特殊的
+ *
+ * 思路：第一轮从左往右排序，第二轮从右往左排序，第三轮从左往右排序...
+ * 类似钟摆，从两端排序
+ */
+public class CocktailSort {
+
+    public static void main(String [] args){
+
+    }
+
+    private static void sort(){
+
+    }
+}
